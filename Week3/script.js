13862fc77f1e05c07d7698c8293cad044b849133
ayulana <masayuln07@gmail.com>
2026-09-22 22:15:12 +0700

    Update script.js

--- Week3/script.js
+++ Week3/script.js
@@ -1,1 +1,17 @@
+const elemenReveal = document.querySelectorAll(".reveal");
 
+const observer = new IntersectionObserver(
+  function (entries) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("tampil");
+        observer.unobserve(entry.target); 
+      }
+    });
+  },
+  { threshold: 0.10 }
+);
+
+elemenReveal.forEach(function (el) {
+  observer.observe(el);
+});
